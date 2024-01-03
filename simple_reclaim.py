36d5d8f2ff09e6c22b5bcd1ffb1c13c3ea293855
Peter <baughgoth@gmail.com>
2024-01-03 11:10:33 +0000

    Update simple_reclaim.py
    Made a bit more flexible, probably better than the targeted version now. No-one seems interested though?

--- simple_reclaim.py
+++ simple_reclaim.py
@@ -3,29 +3,47 @@ import btrfs
 import subprocess
 import argparse
 import sys
+import statistics
 
-parser = argparse.ArgumentParser(description='Targeted balance to free up chunks that are unallocatable because of unbalanced allocations.')
-parser.add_argument('path', help = '<path> to raid1 filesystem')
-parser.add_argument('-d', '--debug', help = 'debug mode', action='store_true')
+parser = argparse.ArgumentParser(description='Targeted balance to free up unallocatable space')
+parser.add_argument('path', help = '<path> to filesystem')
+#parser.add_argument('-d', '--debug', help = 'debug mode', action='store_true')
 args = parser.parse_args()
 
-#chunks is a simple list vaddrs with slices not in the most unalloced dev.
+#chunks is a simple list of vaddrs with slices not in the most filled dev.
 chunks = []
+chunksizes = []
 with btrfs.FileSystem(args.path) as fs:
+    reclaimable = fs.usage().unallocatable_reclaimable
+    if reclaimable == 0:
+        print('Nothing to reclaim')
+        #sys.exit()
     unalloc = []
     for device in fs.devices():
         unalloc.append((btrfs.fs_usage.DevUsage(device).unallocated, device.devid))
     devid = sorted(unalloc, reverse = True)[0][1] #Prohibit dev with most unallocated
     for chunk in fs.chunks():
-        if (chunk.type == 17 and #Only deal with DATA/RAID1
-                chunk.stripes[0].devid != devid and
-                chunk.stripes[1].devid != devid):
-            chunks.append(chunk.vaddr)
+        if chunk.type & btrfs.ctree.BLOCK_GROUP_TYPE_MASK == 1: #Only deal with DATA
+            devs = set()
+            chunksizes.append(chunk.length)
+            for stripe in chunk.stripes:
+                devs.add(stripe.devid)
+            #print(chunk, devs)
+            if devid not in devs:
+                chunks.append(chunk.vaddr)
+                #print('added')
+    chunksize = statistics.mode(chunksizes)
+    print('chunksize', btrfs.utils.pretty_size(chunksize))
+    if reclaimable < chunksize:
+        print('Nothing to reclaim')
+        #sys.exit()
     print('Found', len(chunks), 'chunks not on dev', devid)
-    reclaimable = fs.usage().unallocatable_reclaimable
-    while reclaimable > 0:
+    while reclaimable >= chunksize:
         print('Unallocated reclaimable:', btrfs.utils.pretty_size(reclaimable))
-        vaddr_s = chunks.pop()
+        if chunks:
+            vaddr_s = chunks.pop()
+        else:
+            sys.exit('No chunks left')
         print('Balancing chunk', vaddr_s)
         balance = ['btrfs', 'balance', 'start', '-dvrange='+str(vaddr_s)+'..'+str(vaddr_s+1), args.path]
         #print(balance)
